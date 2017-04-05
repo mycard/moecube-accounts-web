19e3a13d9f1d08fe51c1585f5d3c96433fb509f2
2breakegg <1216023881@qq.com>
2017-04-05 15:51:57 +0800

    i18n

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,11 +1,12 @@
 import { Button } from 'antd';
 import React from 'react';
 import styles from './SubmitButton.css';
+import { FormattedMessage as Format } from 'react-intl';
 
 function SubmitButton(props) {
   return (
     <div className={styles.wrapSubmit}>
-      <Button type="primary" htmlType="submit" size="large" {...props} >提交</Button>
+      <Button type="primary" htmlType="submit" size="large" {...props} ><Format id="save" /></Button>
     </div>
   );
 }
