61521e2395ec6ec0eeb02ab06c82e8de395dec69
zh99998 <zh99998@gmail.com>
2017-04-07 12:30:07 +0800

    env

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -1,39 +1,45 @@
-import React from 'react';
+import { Dropdown, Menu } from 'antd';
 import { connect } from 'dva';
-import styles from './UserPanel.css';
-import { Dropdown, Menu, Icon } from 'antd';
+import { Link } from 'dva/router';
+import React from 'react';
 import { FormattedMessage as Format } from 'react-intl';
-import { Link } from 'dva/router'
 
 const defaultAvatar = require('../../public/default_avatar.jpg');
 
 function UserPanel({ dispatch, user, token }) {
-
   const { avatar } = user;
   const menu = (
     <Menu trigger={['click']}>
       <Menu.Item>
-      {
-        token ? <Link to="/profiles">//i18n 个人中心</Link> : <Link to="/signin">//i18n 登录</Link> 
-      }
+        {
+          // i18n
+          token ? <Link to="/profiles">个人中心</Link> : <Link to="/signin">登录</Link>
+        }
       </Menu.Item>
 
       <Menu.Divider/>
       {
-        token && 
-      <Menu.Item >
-        <div onClick={() => { dispatch({ type: 'auth/signOut' }) }}>
-          <Format id="sign-out" />                          
-        </div>
-      </Menu.Item>
+        token &&
+        <Menu.Item >
+          <div
+            onClick={() => {
+              dispatch({ type: 'auth/signOut' });
+            }}
+          >
+            <Format id="sign-out"/>
+          </div>
+        </Menu.Item>
       }
     </Menu>
-  )
+  );
 
   return (
     <Dropdown overlay={menu} placement="bottomCenter">
-      <div className="ant-dropdown-link" style={{ display: 'flex', alignItems: 'center'}}>
-        <img alt="avatar" src={avatar || defaultAvatar} style={{ borderRadius: '40px', height: '40px', weight: 'auto' }}/>
+      <div className="ant-dropdown-link" style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          alt="avatar" src={avatar || defaultAvatar}
+          style={{ borderRadius: '40px', height: '40px', weight: 'auto' }}
+        />
       </div>
     </Dropdown>
   );
