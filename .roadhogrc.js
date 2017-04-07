389fa3c5713b80396455eb48303ced992990e103
zh99998 <zh99998@gmail.com>
2017-04-07 12:36:32 +0800

    env

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -1,33 +1,32 @@
-
-let publicPath = 'https://cdn01.moecube.com/accounts/'
+let publicPath = 'https://cdn01.moecube.com/accounts/';
 
 const API_ROOT = {
-  test: 'http://192.168.1.9:3000',
-  dev: 'http://114.215.243.95:8082',
-  prod: 'https://api.moeube.com/accounts'
-}
+  development: 'http://192.168.1.9:3000',
+  test: 'http://114.215.243.95:8082',
+  production: 'https://api.moeube.com/accounts'
+};
 
 let defineConf = {
-  apiRoot: API_ROOT[process.env["ENV"]],
-}
+  apiRoot: API_ROOT[process.env['ENV']],
+};
 
 export default {
-  "entry": "src/index.js",
+  'entry': 'src/index.js',
   publicPath,
-  define: {...defineConf},
-  "env": {
-    "development": {
-      "extraBabelPlugins": [
-        "dva-hmr",
-        "transform-runtime",
-        ["import", { "libraryName": "antd", "style": "css" }]
+  define: { ...defineConf },
+  'env': {
+    'development': {
+      'extraBabelPlugins': [
+        'dva-hmr',
+        'transform-runtime',
+        ['import', { 'libraryName': 'antd', 'style': 'css' }]
       ]
     },
-    "production": {
-      "extraBabelPlugins": [
-        "transform-runtime",
-        ["import", { "libraryName": "antd", "style": "css" }]
+    'production': {
+      'extraBabelPlugins': [
+        'transform-runtime',
+        ['import', { 'libraryName': 'antd', 'style': 'css' }]
       ]
     }
   }
-}
+};
